407ac7e86ad3506b660340ffb98f03164555fd65
Ruwan-Ranganath <ruwanranganathz@gmail.com>
2018-02-06 17:07:30 +0530

    updated global security naming

--- global.tf
+++ global.tf
@@ -22,9 +22,20 @@ resource "google_compute_firewall" "ssh" {
 }
 
 
+# Firewall Groups Start =====================================
+
 #Monitoring Security Group
-resource "google_compute_firewall" "monitoring_sg" {
-  name    = "${var.platform-name}-monitoring_sg"
+# Rules Explanation
+# ICMP PING // should be disabled
+# 22 SSH
+# 80 HTTP for Nagios
+# 443  HTTPs for Nagios
+# 9200 Elasticsearch
+# 5043 Logstash
+# 5601 Kibana
+# 5666 Nagios NRPE
+resource "google_compute_firewall" "monitoring" {
+  name    = "${var.platform-name}-monitoring"
   network = "${google_compute_network.platform.name}"
 
   allow {
@@ -39,17 +50,56 @@ resource "google_compute_firewall" "monitoring_sg" {
    source_ranges = ["0.0.0.0/0"]
 }
 
+
+
+#Bastion Security Group
 # Rules Explanation
 # ICMP
 # 22
-# 80 HTTP for Nagios
-# 443  HTTPs for Nagios
-# 9200 Elasticsearch
-# 5043 Logstash
-# 5601 Kibana
-# 5666 Nagios NRPE
+resource "google_compute_firewall" "bastion" {
+  name    = "${var.platform-name}-bastion"
+  network = "${google_compute_network.platform.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+   source_ranges = ["0.0.0.0/0"]
+}
 
 
+
+
+#Nginx Security Group
+# Rules Explanation
+# ICMP
+# 80 HTTP
+# 22 SSH
+# 9200 HTTPS 
+resource "google_compute_firewall" "nginx" {
+  name    = "${var.platform-name}-nginx"
+  network = "${google_compute_network.platform.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "9200"]
+  }
+
+   source_ranges = ["0.0.0.0/0"]
+}
+
+
+
+# Firewall Groups End  =====================================
 
 #Create subnet
 resource "google_compute_subnetwork" "dev" {
