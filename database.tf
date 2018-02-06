3016e1013fb7c9a85e955cb607d32a8c4bc63eb1
Ruwan-Ranganath <ruwanranganathz@gmail.com>
2018-02-06 23:25:33 +0530

    update 1.0

--- database.tf
+++ database.tf
@@ -1,16 +1,25 @@
 resource "google_sql_database_instance" "proddb" {
   name = "database-production"
   region = "${var.gcloud-region}"
+#   database_version = "MYSQL_5_7" // define the db type , default is mysql 5.7
 
   settings {
     tier = "db-f1-micro"
   }
 }
 
-resource "google_sql_database" "users" {
-  name      = "users-db"
-  instance  = "${google_sql_database_instance.proddb.name}"
-  charset   = "latin1"
-  collation = "latin1_swedish_ci"
+resource "google_sql_user" "users" {
+  name     = "${var.sql-user}"
+  instance = "${google_sql_database_instance.proddb.name}"
+  host     = "${var.sql-host}"
+  password = "${var.sql-password}"
 }
+
     
+# # Create Sample Table 
+# resource "google_sql_database" "users" {
+#   name      = "users-db"
+#   instance  = "${google_sql_database_instance.proddb.name}"
+#   charset   = "latin1"
+#   collation = "latin1_swedish_ci"
+# }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "gcloud-region"    { default = "us-west1" }
 variable "gcloud-zone"      { default = "us-west1-a" }
 variable "gcloud-project"   { default = "wso2telcoubeexpansion" }
 variable "platform-name"    { default = "wso2telcoubeexpansion"  }
+
+#mysql user variables 
+variable "sql-user"         { default = "rootadmin"}
+variable "sql-password"     { default = "MyRandomPassword123"} // this should be changed. 
+variable "sql-host"         { default = "connect.wso2telco.com"}
+
